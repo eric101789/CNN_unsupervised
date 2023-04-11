1a16098e0e49a12e90d8c5422a5fe77ef764b06a
eric101789 <eric10179@gmail.com>
2023-04-11 15:33:33 +0800

    建立專案；匯入資料集(dataset: train_set, val_set, test_set)；完成輸出訓練模型(train_model, train_model_LSTM)

---
+++ train.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+from keras import Sequential
+from keras.layers import MaxPooling2D, Convolution2D, Flatten, Reshape, LSTM, Dense
+
+# 設定圖像大小和批量大小
+image_size = (64, 64, 3)
+batch_size = 32
+
+# 讀取訓練集圖像
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255,
+                                                                shear_range=0.2,
+                                                                zoom_range=0.2,
+                                                                horizontal_flip=True)
+train_generator = train_datagen.flow_from_directory(
+    'dataset/train_set',
+    target_size=(64, 64),
+    batch_size=batch_size,
+    class_mode='binary')
+
+val_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. / 255)
+val_generator = val_datagen.flow_from_directory(
+    'dataset/val_set',
+    target_size=(64, 64),
+    batch_size=batch_size,
+    class_mode='binary')
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation='relu'))
+
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Convolution2D(32, 3, 3, activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Reshaping the output of the previous layer to a 3D tensor
+classifier.add(Reshape((1, -1)))
+
+# Add a LSTM Layer
+classifier.add(LSTM(units=128))
+
+# Step 4 - Full connection
+# classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=1, activation='sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+classifier.fit(train_generator,
+               epochs=40,
+               steps_per_epoch=int(10500 // batch_size),
+               validation_data=val_generator,
+               validation_steps=int(3000 / batch_size))
+
+classifier.save('model/train_model_LSTM.h5')
